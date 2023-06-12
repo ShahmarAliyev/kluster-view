fef00145007d69a39d751c736297cabe91478356
Shahmar Aliyev <shahmaraliyev@Shahmars-MacBook-Pro.local>
2023-06-12 14:07:06 -0400

    setup starting files and folders

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='logo'>KV</div>
+      <div className='options'></div>
+    </div>
+  );
+};
+
+export default Navbar;
